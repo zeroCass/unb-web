da52fdc8752d3ee68a771b71fb8ddadc08481e2c
Maylla Krislainy <mayllak@hotmail.com>
2023-07-03 17:48:28 -0300

    feat: pagina questoes (issue #1)
    criação da pagina  onde o professor pode visualizar todas as suas questões.

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -3,4 +3,5 @@ def blueprints():
     from .qrcode_controller import bp as qrcode_bp
     from .aulas_controller import bp as aulas_bp
     from .exames_controller import bp as exames_bp
-    return [turmas_bp, qrcode_bp]
+    from .questoes_controller import bp as questoes_bp
+    return [turmas_bp, qrcode_bp, questoes_bp]
---
+++ app/controllers/questoes_controller.py
@@ -0,0 +1,21 @@
+from ..webapp import db
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
+from ..models import Questao, Professor, QuestaoMultiplaEscolha
+
+bp = Blueprint("questoes", __name__)
+
+@bp.route("/professor/<int:user_id>", methods=["GET"])
+@login_required
+def index(user_id):
+    # Verificar se o usuário logado é um professor
+    if not isinstance(current_user, Professor):
+        flash("Acesso não autorizado")
+        return render_template("index.jinja2", user=current_user)
+    
+    # Obtendo as questões relacionadas ao usuário
+    questoes = Questao.query.filter_by(professor_id=user_id).all()
+    questoes_multipla_escolha = QuestaoMultiplaEscolha.query.filter_by(professor_id=user_id).all()
+
+    return render_template('questoes/index.jinja2', questoes=questoes, questoes_multipla_escolha=questoes_multipla_escolha)
+
