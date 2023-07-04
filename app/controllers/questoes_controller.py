9b5c365e86294118ea359f0e12893e8bce6a3ad6
Maylla Krislainy <mayllak@hotmail.com>
2023-07-04 17:55:48 -0300

    fix: criar questao (issue #1)
    criar questão é finalmente possível, podendo diferenciar pra cada tipo de questão.

--- app/controllers/questoes_controller.py
+++ app/controllers/questoes_controller.py
@@ -27,4 +27,29 @@ def new(user_id):
 @bp.route("/professor/<int:user_id>/create", methods=["POST"])
 @login_required
 def create(user_id):
-    return render_template("questoes/new.jinja2", user_id=user_id)
+
+    enunciado = request.form['enunciado']
+    tipo_questao = request.form['tipo_questao']
+    opcao_a = request.form.get('opcao_a')
+    opcao_b = request.form.get('opcao_b')
+    opcao_c = request.form.get('opcao_c')
+    opcao_d = request.form.get('opcao_d')
+    resposta = request.form['resposta']
+    professor_id = user_id
+
+    # Cria uma nova instância da classe Questao
+    if(tipo_questao == "multipla_escolha"):
+        nova_questao = QuestaoMultiplaEscolha(enunciado=enunciado, tipo_questao=tipo_questao, opcao_a=opcao_a, opcao_b=opcao_b, opcao_c=opcao_c, opcao_d=opcao_d, resposta=resposta, professor_id=professor_id)
+    else:
+        nova_questao = Questao(enunciado=enunciado, tipo_questao=tipo_questao, resposta=resposta, professor_id=professor_id)
+    
+    # Salva a nova questão no banco de dados
+    try:
+        db.session.add(nova_questao)
+        db.session.commit()
+        flash("Questao criada")
+    except Exception:
+        db.session.rollback()
+        flash("Erro ao criar Questao")
+
+    return render_template('questoes/index.jinja2', questoes=questoes, questoes_multipla_escolha=questoes_multipla_escolha)
