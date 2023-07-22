731fae582501f573a497307a242942112262718a
Mateus Valerio <mateusvalerio1@gmail.com>
2023-07-22 17:41:08 -0300

    refactor: refatação das controllers

--- app/controllers/presenca_controller.py
+++ app/controllers/presenca_controller.py
@@ -6,24 +6,34 @@ from app.utils.qrcode import gerar_qrcode, qrcode_isvalid
 from datetime import datetime
 from flask_login import login_required, current_user
 import base64
-
+from typing import Union
 
 bp = Blueprint("presenca", __name__)
 
 
-def register_blueprint(parent_blueprint: Blueprint):
+def register_blueprint(parent_blueprint: Blueprint)-> None:
+    """Registra essa rota (blueprint) como parte
+    da rota do pai. Dessa forma, o rota desse blueprint será
+    composta com a rota do pai como prefixo
+
+    Args:
+        O blueprint do pai
+    """
     parent_blueprint.register_blueprint(
         bp, url_prefix=f"/<int:aula_id>/presenca")
 
 
 @bp.route("/", methods=["POST", "GET"])
 @login_required
-def registrar_presenca(turma_id, aula_id):
-    """
-    registra a presenca de uma estudante, caso não haja regristo
+def registrar_presenca(turma_id: int, aula_id: int) -> Union[redirect,render_template]:
+    """Registra a presenca de uma estudante, caso não haja regristo
 
-    :return: redireciona para a pagina de exibicao de turma com 
-    alguma mensagme de flash indicando o status da operacao
+    Args:
+        turma_id: id da turma
+        aula_id: id da aula
+    Returns: 
+        redireciona para a pagina de exibicao de turma com 
+        alguma mensagme de flash indicando o status da operacao
     """
     if request.method == "GET":
         return render_template("qrcode.jinja2", turma_id=turma_id, aula_id=aula_id)
@@ -40,7 +50,7 @@ def registrar_presenca(turma_id, aula_id):
             flash("Presença já foi registrada", category="warning")
         else:
             qrcode_content = request.form.get("qrcode-content")
-            print(f"PRESENCA: POST {qrcode_content}")
+            # print(f"PRESENCA: POST {qrcode_content}")
             if qrcode_isvalid(qrcode_content, aula_id):
                 presenca = Presenca(
                     aula_id=aula_id, data=data_atual, estudante_id=estudante_id)
@@ -57,7 +67,17 @@ def registrar_presenca(turma_id, aula_id):
         
 @bp.route("/estudantes", methods=["GET"])
 @login_required
-def listar_presencas(turma_id, aula_id):
+def listar_presencas(turma_id: int, aula_id: int) -> render_template:
+    """Renderiza a página contendo todos os alunos que assinaram a 
+    presençã de uma determina aula
+
+    Args:
+        turma_id: id da turma 
+        aula_id: id da aula
+    Returns:
+        Redireciona para a página de presenças
+    """
+
     aula = Aula.query.get(aula_id)
     estudantes = Estudante.query.all()
     presencas = Presenca.query.filter_by(aula_id=aula_id).all()
@@ -67,10 +87,14 @@ def listar_presencas(turma_id, aula_id):
 
 @bp.route("/qrcode", methods=["GET"])
 @login_required
-def qrcode(turma_id, aula_id):
-    """
-    gera uma imagem qrcode baseado no token da aula
-    :return: imagem para ser renderizado no template jinja
+def qrcode(turma_id: int, aula_id: int) -> send_file:
+    """Gera uma imagem qrcode baseado no token da aula
+    
+    Args:
+        turma_id: id da turma
+        aula_id: id da aula
+    Returns: 
+        imagem para ser renderizado no template jinja
     """
 
     aula = Aula.query.filter_by(id=aula_id).first()
@@ -82,10 +106,14 @@ def qrcode(turma_id, aula_id):
 
 @bp.route("/qrcode_page", methods=["GET"])
 @login_required
-def qrcode_page(turma_id, aula_id):
-    """
-    gera uma imagem qrcode baseado no token da aula
-    :return: template jinja com imagem em base64 como parametro
+def qrcode_page(turma_id: int, aula_id: int) -> render_template:
+    """Gera uma imagem qrcode baseado no token da aula
+    
+    Args:
+        turma_id: id da turma
+        aula_id: id da aula
+    Returns: 
+        template jinja com imagem em base64 como parametro
     """
 
     aula = Aula.query.filter_by(id=aula_id).first()
